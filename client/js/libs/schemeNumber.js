92b725d1fc00af870b64dddafe5e73841603c4bf
Jeff Miller <jeffmax@gmail.com>
2012-11-09 15:02:36 -0500

    Adding backup forEach impl.

--- client/js/libs/schemeNumber.js
+++ client/js/libs/schemeNumber.js
@@ -13,6 +13,15 @@
         <biginteger.js> for <BigInteger>
  */
 
+
+if ( !Array.prototype.forEach ) {
+  Array.prototype.forEach = function(fn, scope) {
+    for(var i = 0, len = this.length; i < len; ++i) {
+      fn.call(scope, this[i], i, this);
+    }
+  }
+}
+
 // Grab the BigInteger library.
 var BigInteger;
 if (typeof require !== "undefined")
