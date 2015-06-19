30b33eac83ffaf25936abc32614f4bd5ee729dd6
Jeffrey Miller <millerjm1@email.chop.edu>
2015-06-19 10:25:35 -0400

    Make DJANGO_ADMIN_PASSWORD optional

--- scripts/http.sh
+++ scripts/http.sh
@@ -3,15 +3,16 @@
 APP_DIR=/opt/app/
 cd $APP_DIR
 
+
 if [ "$LOGIN_ENABLED" = "1" ]; then
-    DJANGO_SETTINGS_MODULE='server.settings' django-admin.py syncdb
-    if [ "$LOGIN_ENABLED" = "1" ]; then
-        DJANGO_SETTINGS_MODULE='server.settings' django-admin.py syncdb --noinput
-        if [ -z "$DJANGO_ADMIN_USER" ] && [ -z "$DJANGO_ADMIN_EMAIL" ] && [-z "$DJANGO_ADMIN_PASSWORD"]; then 
-            echo "from django.contrib.auth.models import User; User.objects.create_superuser('"$DJANGO_ADMIN_USER"', '"$DJANGO_ADMIN_EMAIL"', '"$DJANGO_ADMIN_PASSWORD"')" | DJANGO_SETTINGS_MODULE='server.settings' django-admin.py shell
-        fi
-    fi
+    DJANGO_SETTINGS_MODULE='server.settings' django-admin.py syncdb --noinput
+    if [ -z "$DJANGO_ADMIN_USER" ] && [ -z "$DJANGO_ADMIN_EMAIL" ] && [-z "$DJANGO_ADMIN_PASSWORD"]; then 
+        echo "from django.contrib.auth.models import User; User.objects.create_superuser('"$DJANGO_ADMIN_USER"', '"$DJANGO_ADMIN_EMAIL"', '"$DJANGO_ADMIN_PASSWORD"')" | DJANGO_SETTINGS_MODULE='server.settings' django-admin.py shell
+    elif [ -z "$DJANGO_ADMIN_USER" ] && [ -z "$DJANGO_ADMIN_EMAIL" ]; then
+        DJANGO_SETTINGS_MODULE='server.settings' django-admin.py createsuperuser --username=$DJANGO_ADMIN_USER --email=$DJANGO_ADMIN_EMAIL
+    fi 
 fi
+
 
 # Write the config file that the JS client requires
 python /opt/app/scripts/write_config.py
