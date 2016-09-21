33301a4257b760efd54d20db5e7759c8368439f9
Jeff Miller <millerjm1@email.chop.edu>
2016-09-21 10:26:39 -0400

    Use window.location to load reportaconcern.css

--- client/js/reportaconcern.js
+++ client/js/reportaconcern.js
@@ -7,7 +7,7 @@ define(["jquery","underscore","utils","config","libs/jquery-ui-min"], function($
         var link = document.createElement("link");
         link.type = "text/css";
         link.rel = "stylesheet";
-        link.href = url;
+        link.href = window.location.pathname + "/" + url;
         document.getElementsByTagName("head")[0].appendChild(link);
     }
     loadCss("css/reportaconcern.css");
