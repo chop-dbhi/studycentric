162d8076731746d762ef1445fc2e1c83cc2a11ce
Jeff Miller <millerjm1@email.chop.edu>
2016-09-21 09:43:48 -0400

    Serve index.html

--- scripts/http.sh
+++ scripts/http.sh
@@ -21,7 +21,7 @@ python /opt/app/scripts/write_config.py
 
 
 if [ -n "$HTTPS_ON" ]; then
-    exec /usr/local/bin/uwsgi --die-on-term --https 0.0.0.0:8443,$SSL_CERT_PATH/certificate.crt,$SSL_CERT_PATH/private.key -p 2 -b 32768 -T --master --max-requests 5000 --static-map /static=/opt/app/client --module wsgi:application
+    exec /usr/local/bin/uwsgi --die-on-term --https 0.0.0.0:8443,$SSL_CERT_PATH/certificate.crt,$SSL_CERT_PATH/private.key -p 2 -b 32768 -T --master --max-requests 5000 --static-map /static=/opt/app/client --static-index index.html --module wsgi:application
 else
-    exec /usr/local/bin/uwsgi --die-on-term --http-socket :8000 -p 4 -b 32768 -T --master --max-requests 5000 --static-map /static=/opt/app/client --module wsgi:application
+    exec /usr/local/bin/uwsgi --die-on-term --http-socket :8000 -p 4 -b 32768 -T --master --max-requests 5000 --static-map /static=/opt/app/client --static-index index.html --module wsgi:application
 fi
